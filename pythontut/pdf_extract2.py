9d4b2abe7f52b2543d9678f5d4a9faf581fae4dc
craggak87 <alex.huza@gmail.com>
2022-01-11 12:58:43 +0000

    working in Py2PDF

--- pythontut/pdf_extract2.py
+++ pythontut/pdf_extract2.py
@@ -5,9 +5,63 @@ from tkinter.filedialog import askopenfile
 from functions import display_logo, display_textbox, extract_images, display_icon, resize_image, display_images
 
 page_contents = []
+all_images = []
+imge_idx = [0]
+displayed_img = []
+
+def right_arrow(all_images, current_image, what_text):
+    if imge_idx[-1] < len(all_images) -1:
+        new_idx = imge_idx[-1] + 1
+        imge_idx.pop()
+        imge_idx.append(new_idx)
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+        new_img = all_images[imge_idx[-1]]
+        current_image = display_images(new_img)
+        displayed_img.append(current_image)
+        what_text.set("image" + str(imge_idx[-1]+1) + "out of "+ str(len(all_images)))
+    elif imge_idx == len(all_images) -1:
+        print("index out of range")
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+
+def left_arrow(all_images, current_image, what_text):
+    if imge_idx[-1] >=1:
+        new_idx = imge_idx[-1] - 1
+        imge_idx.pop()
+        imge_idx.append(new_idx)
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+        new_img = all_images[imge_idx[-1]]
+        current_image = display_images(new_img)
+        displayed_img.append(current_image)
+        what_text.set("image" + str(imge_idx[-1]+1) + "out of "+ str(len(all_images)))
+    elif imge_idx == len(all_images) -1:
+        print("index out of range")
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+
 def copy_text(content):
     root.clipboard_clear()
     root.clipboard_append(content[-1])
+
+def save_all(images):
+    counter = 1
+    for i in images:
+        if i.mode != "RGB":
+            i = i.convert("RGB")
+        i.save("image" + str(counter) + ".png", format="png")
+        counter += 1
+
+def save_image(i):
+    if i.mode != "RGB":
+        i = i.convert("RGB")
+    i.save("image.png", format="png")
+
 root = Tk()
 root.geometry('+%d+%d'%(1250,10))
 
@@ -16,6 +70,9 @@ main_content = Frame(root, width=800, height=250, bg="#20bebe")
 main_content.grid(columnspan=3, rowspan=2, row=4)
 
 def open_file():
+    for i in imge_idx:
+        imge_idx.pop()
+    imge_idx.append(0)
     browse_text.set("Loading...")
     file = askopenfile(parent=root, mode='rb', filetypes=[("Pdf file", "*.pdf")])
     if file:
@@ -25,10 +82,21 @@ def open_file():
         #page_content = page_content.encode('cp1252')
         page_content = page_content.replace('\u2122', "'")
         page_contents.append(page_content)
-        images = extract_images(page)
-        img = images[0]
-        display_images(img)
 
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+
+        for i in range(0, len(all_images)):
+            all_images.pop()
+
+        images = extract_images(page)
+        for i in images:
+            all_images.append(i)
+        img = images[imge_idx[-1]]
+
+        current_image = display_images(img)
+        displayed_img.append(current_image)
         #show text box on row 2 col 0
         display_textbox(page_content, 4, 0, root)
 
@@ -42,18 +110,20 @@ def open_file():
         img_menu = Frame(root, width=800, height=60, bg="#c8c8c8")
         img_menu.grid(columnspan=3, rowspan=1, row=2)
 
-        what_img = Label(root, text="image 1 of 5", font=("shanti", 10))
+        what_text = StringVar()
+        what_img = Label(root, textvariable=what_text, font=("shanti", 10))
+        what_text.set("image" + str(imge_idx[-1]+1) + "out of "+ str(len(all_images)))
         what_img.grid(row=2, column=1)
 
-        display_icon('arrow_l.png', 2, 0, E)
-        display_icon('arrow_r.png', 2, 2, W)
+        display_icon('arrow_l.png', 2, 0, E, lambda:left_arrow(all_images, current_image, what_text))
+        display_icon('arrow_r.png', 2, 2, W, lambda:right_arrow(all_images, current_image, what_text))
 
         save_img = Frame(root, width=800, height=60, bg="#c8c8c8")
         save_img.grid(columnspan=3, rowspan=1, row=3)
 
         copyText_btn = Button(root, text="copy text", command=lambda:copy_text(page_contents), font=("shanti", 10), height=1, width=15)
-        saveAll_btn = Button(root, text="Save all images", font=("shanti", 10), height=1, width=15)
-        save_btn = Button(root, text="Save image", font=("shanti", 10), height=1, width=15)
+        saveAll_btn = Button(root, text="Save all images", command=lambda:save_all(all_images), font=("shanti", 10), height=1, width=15)
+        save_btn = Button(root, text="Save image", command=lambda:save_image(all_images[imge_idx[-1]]), font=("shanti", 10), height=1, width=15)
         copyText_btn.grid(row=3, column=0)
         saveAll_btn.grid(row=3, column=1)
         save_btn.grid(row=3, column=2)
